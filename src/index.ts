48ae0fee79d82236ff746a866bf43530ed169326
Ru Chern Chong <hello@ruchern.dev>
2025-09-04 23:05:23 +0800

    feat: add middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { Hono } from "hono";
+import { compress } from "hono/compress";
+import { etag } from "hono/etag";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+import { secureHeaders } from "hono/secure-headers";
 
 const app = new Hono();
 
@@ -6,6 +11,12 @@ const welcomeStrings = [
   "Hello Hono!",
   "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/hono",
 ];
+
+app.use(compress());
+app.use("*", etag());
+app.use(logger());
+app.use(prettyJSON());
+app.use(secureHeaders());
 
 app.get("/", (c) => {
   return c.json({ message: welcomeStrings.join("\n\n") });
